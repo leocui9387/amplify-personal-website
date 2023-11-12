ad3618e54f0c1452ac9704263e2c421f3e909567
leocui9387 <Basic.Bear@outlook.com>
2023-11-12 17:24:37 -0500

    Added CV calling code.

--- src/components/CurriculumVitae.js
+++ src/components/CurriculumVitae.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown'
-import cvText from "./references/cv.md"
+
 
 export class CurriculumVitae extends Component {
     static displayName = CurriculumVitae.name;
@@ -11,16 +11,16 @@ export class CurriculumVitae extends Component {
             const [mdText, setText] = React.useState("error");
 
             React.useEffect(() => {
-                fetch(cvText).then(f => f.text()).then(t => setText(t));
-                
-            },[]);
+                fetch("https://basic-bear-engineering.s3.amazonaws.com/cv.md").then(f => f.text()).then(t => setText(t));
 
-            
+            }, []);
+
+
             return (<ReactMarkdown children={mdText} />);
         }
 
         return (
-            <Renderer/>
+            <Renderer />
         );
-  }
+    }
 }
